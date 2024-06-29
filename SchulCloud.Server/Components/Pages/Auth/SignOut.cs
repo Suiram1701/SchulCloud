9355c1475c52b08192e52aaab9d0fbaff709cd05
Marius Kaufman <marius.kaufman@web.de>
2024-06-29 15:21:38 +0200

    Renamed every injected service in pascal case because they are properties not fields. Used a raw .cs file for the SignOut component because it doesn't contain any markup code. Improved the naming and positioning of some files.

---
+++ SchulCloud.Server/Components/Pages/Auth/SignOut.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using SchulCloud.Database.Models;
+
+namespace SchulCloud.Server.Components.Pages.Auth;
+
+[AllowAnonymous]
+[Route("/auth/signOut")]
+public class SignOut : ComponentBase
+{
+    [Inject]
+    private ILogger<SignOut> Logger { get; set; } = default!;
+
+    [Inject]
+    private SignInManager<User> SignInManager { get; set; } = default!;
+
+    [Inject]
+    private UserManager<User> UserManager { get; set;} = default!;
+
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = default!;
+
+    [CascadingParameter]
+    private HttpContext? HttpContext { get; set; }
+
+    [CascadingParameter]
+    private Task<AuthenticationState> AuthenticationState { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        AuthenticationState state = await AuthenticationState.ConfigureAwait(false);
+
+        if (SignInManager.IsSignedIn(state.User))
+        {
+            if (HttpContext is null)
+            {
+                NavigationManager.Refresh(forceReload: true);     // Forces the client to send a GET request which is required for the sign out.
+                return;
+            }
+
+            await SignInManager.SignOutAsync().ConfigureAwait(false);
+
+            string userId = UserManager.GetUserId(state.User)!;
+            Logger.LogDebug("User {id} signed out.", userId);
+        }
+
+        NavigationManager.NavigateTo("/auth/signIn");
+    }
+
+    protected override bool ShouldRender() => false;
+}
