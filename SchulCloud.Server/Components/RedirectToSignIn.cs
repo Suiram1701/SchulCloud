dc52c9ff6f920f60f0faeb6c9a6467eb89ef00d0
Suiram1701 <110390261+Suiram1701@users.noreply.github.com>
2024-08-06 18:54:59 +0200

    -Fix: Fixed unauthorized navigation error ( #1 ) and improved navigation logic in general.

---
+++ SchulCloud.Server/Components/RedirectToSignIn.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using SchulCloud.Server.Extensions;
+
+namespace SchulCloud.Server.Components;
+
+public class RedirectToSignIn : ComponentBase
+{
+    #region Injections
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = default!;
+    #endregion
+
+    protected override void OnInitialized()
+    {
+        NavigationManager.NavigateToSignIn(returnUrl: NavigationManager.GetRelativeUri());
+    }
+}
